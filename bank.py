612dca09ec0706b5f1907c8b45d403291f2796c8
AngelaSophia <ayugisophiaangela@gmail.com>
2021-05-24 20:50:31 +0300

    happy

--- bank.py
+++ bank.py
@@ -10,4 +10,34 @@ class Bank:
     def current_balance(self):
          return f"Hello my name is {self.name} and my current balance is {500000-self.balance}"
 
+class Account:
+    def __init__(self,name,phonenumber,balance):
+        self.name=name
+        self.phonenumber=phonenumber
+        self.balance=balance
+        self.borrow=0
+    def showbalance(self):
+        return f"Hello, {self.name},your balance is {self.balance}"
+
+    def deposite(self,amount):
+        if amount<0:
+
+            return f"cannot add any amount"
+        else:
+             self.balance +=amount
+             return self.showbalance()
+             
+    def withdraw(self,amount):
+        if amount>self.balance:
+            return f"your balance is {self.balance} you cannot withdraw {amount}"
+        else:
+               self.balance-=amount
+               return self.showbalance()
+    def add_laon(self,amount):
+        return f"my amount is {amount}"
+    def replayloan(self,amount):
+        return f"my amount is"
+       
+
+
 
--- student.py
+++ student.py
@@ -1,9 +1,22 @@
 class Student:
     school="AkiraChix"
-    def __init__(self,name,age):
-        self.name=name
+    def __init__(self,firstName,lastName,age):
+        self.firstName=firstName
+        self.lastName=lastName
         self.age=age
     def speak(self):
             return f"Hello class, my name is {self.name}"
     def year_of_birth(self):
             return f"Hello my name is {self.name} born in {2021-self.age}"
+    def greet(self):
+        return f"Hello {self.firstName},welcome to {self.school}"
+    def year_of_birth(self):
+        return f"Hello {self.firstName},welcome to {self.school},am {2021-self.age}"
+    def initial(self):
+        return f"Hello {self.firstName},your initial are {self.firstName[0]},welcome to {self.school}"
+
+        
+
+
+
+
